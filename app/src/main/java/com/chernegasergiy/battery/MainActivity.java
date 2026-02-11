837247ea8804fe806bd44fd6697cef2308fc4cde
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-02-11 14:04:13 +0000

    feat: Redesign UI with Material Design 3

--- app/src/main/java/com/chernegasergiy/battery/MainActivity.java
+++ app/src/main/java/com/chernegasergiy/battery/MainActivity.java
@@ -5,17 +5,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.chernegasergiy.battery.fragment.StatusFragment;
 import com.chernegasergiy.battery.fragment.AboutFragment;
 
 public class MainActivity extends AppCompatActivity {
     private StatusFragment statusFragment;
     private AboutFragment aboutFragment;
+    private MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         statusFragment = new StatusFragment();
         aboutFragment = new AboutFragment();
@@ -24,21 +29,24 @@ public class MainActivity extends AppCompatActivity {
         bottomNav.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.nav_status) {
-                loadFragment(statusFragment);
+                loadFragment(statusFragment, "Battery Status");
                 return true;
             } else if (itemId == R.id.nav_about) {
-                loadFragment(aboutFragment);
+                loadFragment(aboutFragment, "About");
                 return true;
             }
             return false;
         });
 
         if (savedInstanceState == null) {
-            loadFragment(statusFragment);
+            loadFragment(statusFragment, "Battery Status");
         }
     }
 
-    private void loadFragment(Fragment fragment) {
+    private void loadFragment(Fragment fragment, String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit();
