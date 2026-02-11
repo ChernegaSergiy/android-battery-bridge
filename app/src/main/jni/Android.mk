590364103b776afee78d48a50a8c0fda6274dd2b
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-02-11 05:27:33 +0000

    chore: Remove old JNI files

--- app/src/main/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := battery-jni
-LOCAL_SRC_FILES := battery_jni.cpp
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_LDLIBS := -llog
-include $(BUILD_SHARED_LIBRARY)
