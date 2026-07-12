44791b53c0ea1b1ee7eb8ec5f2b75c4c72fd0f37
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-12 19:34:31 +0300

    feat: Broadcast intent when settings change in SettingsActivity

--- app/src/main/java/com/chernegasergiy/battery/ui/SettingsActivity.java
+++ app/src/main/java/com/chernegasergiy/battery/ui/SettingsActivity.java
@@ -4,7 +4,13 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import com.chernegasergiy.battery.R;
 
-public class SettingsActivity extends PreferenceActivity {
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    public static final String ACTION_SETTINGS_CHANGED = "com.chernegasergiy.battery.SETTINGS_CHANGED";
+    public static final String EXTRA_SETTING_KEY = "setting_key";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +19,25 @@ public class SettingsActivity extends PreferenceActivity {
         if (getActionBar() != null) {
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Intent intent = new Intent(ACTION_SETTINGS_CHANGED);
+        intent.putExtra(EXTRA_SETTING_KEY, key);
+        sendBroadcast(intent);
     }
     
     @Override
