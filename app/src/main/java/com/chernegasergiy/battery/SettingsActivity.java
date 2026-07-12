01da0eb1e78b94430fa760ab5309b52cf0cd3f2f
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-12 18:11:44 +0300

    feat: Add Up navigation button to Settings screen

--- app/src/main/java/com/chernegasergiy/battery/SettingsActivity.java
+++ app/src/main/java/com/chernegasergiy/battery/SettingsActivity.java
@@ -8,5 +8,18 @@ public class SettingsActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
